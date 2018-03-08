71bf7cf54c9944f09fccf3d002f0a94b5fffdc30
Tinusw <tinus@3months.com>
2018-03-08 23:30:39 +0100

    expanding controller to handle a form POST submit -> pug add page plus mixin for form

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
-
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore)
 module.exports = router;
