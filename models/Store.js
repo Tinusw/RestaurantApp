e7ef25913bd8a6f0c0f9ad443d5a56f820e03510
Tinusw <tinus@3months.com>
2018-03-09 20:23:34 +0100

    learning about dope mongoDB data types for location

--- models/Store.js
+++ models/Store.js
@@ -13,7 +13,25 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'Please supply co-ordinates'
+    }],
+    address: {
+      type: String,
+      required: 'Please supply an address'
+    }
+  }
 });
 
 storeSchema.pre('save', function(next) {
