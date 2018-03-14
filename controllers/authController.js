d7d635651a6dc56359c001ff0c0dd3891ad31ee7
Tinusw <tinus@3months.com>
2018-03-14 22:19:26 +0100

    logout path

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,10 @@ exports.login = passport.authenticate('local', {
   failureFlash: 'Failed login!',
   successRedirect: '/',
   successFlash: 'You are now logged in!',
-})
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'logged out');
+  res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,9 @@ router.get('/register', userController.registerForm);
 router.post('/register',
   userController.validateRegister,
   userController.register,
-  authController.login)
+  authController.login
+);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
