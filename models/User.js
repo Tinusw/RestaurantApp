67dd35a90f1263e3d31802f2baeef758386b4082
Tinusw <tinus@3months.com>
2018-03-15 22:28:43 +0100

    pull user profile image from gravatar

--- models/User.js
+++ models/User.js
@@ -26,6 +26,11 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.virtual('gravatar').get(function(){
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`
+})
+
 // Middleware to add authentication via passport.js
 userSchema.plugin(passport, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
